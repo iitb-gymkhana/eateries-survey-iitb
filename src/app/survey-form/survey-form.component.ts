3d72ddc7f067eb03a8c7cf4f9885d707e6be52e6
Rohit Prasad <rohitrp3@gmail.com>
2018-06-10 00:36:51 +0530

    Initialize form variables

---
+++ src/app/survey-form/survey-form.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-survey-form',
+  templateUrl: './survey-form.component.html',
+  styleUrls: ['./survey-form.component.css']
+})
+export class SurveyFormComponent implements OnInit {
+
+  hostels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16'];
+  frequencies = ['Almost daily', '2-3 times a week', 'Almost every week', 'Less than once a week'];
+  foodItems = ['Tea/Coffee',
+    'Soups',
+    'Milkshakes',
+    'Momos',
+    'Parathas',
+    'South indian dishes',
+    'Maggi',
+    'Veg frankies',
+    'Chicken frankies',
+    'Veg rice and noodles',
+    'Chicken rice and noodles',
+    'Veg Main Course',
+    'Paneer Main course',
+    'Chicken Main Course',
+    'Egg items',
+    'Sandwiches',
+    'Aamras',
+    'Nimbu pani',
+    'Patties/rolls'];
+  hygieneLevels = ['Cleanliness of utensils',	
+    'Cleanliness of Tables',
+    'Cleanliness of cook',
+    'Overall outlet cleanliness'];
+  absenceMenuItems = ['Once', 'Two or three times a week', 'Rarely'];
+  otherParameters = ['Staff Behaviour', 'Serving Time', 'Overall Experience'];
+  ratings = ['1', '2', '3', '4', '5', 'No idea'];  
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
