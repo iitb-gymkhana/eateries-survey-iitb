bad65b49cd8d088940bbc248fb2b9d1ff524aea0
Rohit Prasad <rohitrp3@gmail.com>
2018-06-10 00:57:28 +0530

    Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
@@ -10,7 +11,8 @@ import { SurveyFormComponent } from './survey-form/survey-form.component';
     SurveyFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
