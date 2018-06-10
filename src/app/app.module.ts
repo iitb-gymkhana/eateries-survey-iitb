d51bd361adc333fd07ed323b99972ce3928de330
Rohit Prasad <rohitrp3@gmail.com>
2018-06-11 02:05:53 +0530

    Define header and footer components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,19 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
 import { HttpClientModule } from "@angular/common/http";
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { SurveyFormComponent } from './survey-form/survey-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SurveyFormComponent
+    SurveyFormComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +29,8 @@ import { SurveyFormComponent } from './survey-form/survey-form.component';
     MatButtonModule,
     MatSelectModule,
     MatRadioModule,
-    HttpClientModule
+    HttpClientModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
