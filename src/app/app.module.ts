2d076322250ada5e124a537f92e584839a3e1676
Rohit Prasad <rohitrp3@gmail.com>
2018-06-14 18:02:56 +0530

    Change background color of bar charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
--- src/app/barchart/barchart.component.ts
+++ src/app/barchart/barchart.component.ts
@@ -10,8 +10,29 @@ export class BarchartComponent implements OnInit, AfterViewInit {
   @Input() data: Object;
   @Input() canvasId: string;
   @Input() numData: number;
+  @Input() index: number;
 
   chart = [];
+
+  backgroundColor = [
+    'rgba(255, 99, 132, 0.2)',
+    'rgba(255, 159, 64, 0.2)',
+    'rgba(255, 205, 86, 0.2)',
+    'rgba(75, 192, 192, 0.2)',
+    'rgba(54, 162, 235, 0.2)',
+    'rgba(153, 102, 255, 0.2)',
+    'rgba(201, 203, 207, 0.2)'
+  ];
+
+  borderColor = [
+    'rgb(255, 99, 132)',
+    'rgb(255, 159, 64)',
+    'rgb(255, 205, 86)',
+    'rgb(75, 192, 192)',
+    'rgb(54, 162, 235)',
+    'rgb(153, 102, 255)',
+    'rgb(201, 203, 207)'
+  ];
 
   constructor() { }
 
@@ -35,7 +56,9 @@ export class BarchartComponent implements OnInit, AfterViewInit {
           datasets: [{
               label: this.data['title'],
               data: dataset,
-              borderWidth: 1
+              borderWidth: 1,
+              backgroundColor: this.backgroundColor[this.index % this.backgroundColor.length],
+              borderColor: this.borderColor[this.index % this.borderColor.length]
           }]
       },
       options: {
