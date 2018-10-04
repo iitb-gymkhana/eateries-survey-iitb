c343acaba55bb3eabd46a29916fcdf945520cecb
Rohit Prasad <rohitrp3@gmail.com>
2018-10-04 17:14:57 +0530

    Fix incorrect number of data being assigned for bar charts

--- src/app/chart/chart.component.ts
+++ src/app/chart/chart.component.ts
@@ -21,7 +21,7 @@ export class ChartComponent implements OnInit {
     if (fields.length !== 0) {
       const _values = Object.values(this.surveyData[fields[0]]);
       for (let i = 0; i < _values.length; i++) {
-        this.numData = +(_values[i]);
+        this.numData += +(_values[i]);
       }
 
       for (let i = 0; i < fields.length; i++) {
